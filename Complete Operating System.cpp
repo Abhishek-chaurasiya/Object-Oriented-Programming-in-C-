59b4fa49426827276dbdf8e32d5fba449c883850
Abhishek Chaurasiya <107196798+Abhishek-chaurasiya@users.noreply.github.com>
2023-08-05 17:19:08 +0530

    Create Complete Operating System.cpp

---
+++ Complete Operating System.cpp
@@ -0,0 +1,6 @@
+MEMORY MANAGEMENT  - 
+  It is all aboubt managing finite of main memory to increase the degree of multiprogramming to its most optimal level.
+   1.Fixed Partitioning of Memory(Contagious memory allocation): Main memory is distributed in some amount of sizes like 2mb,4mb,10mb,etc.
+    But there is problem that if a process of size 1 mb is running in 4mb section 3 mb is wasted so this is called internal fragmentation.
+    other problem limitation of problem size. 
+    degree of multiprogramming is limited and fixed .
