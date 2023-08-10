5a449754da64556d3362a7e42bd6799a6e7b119f
Abhishek Chaurasiya <107196798+Abhishek-chaurasiya@users.noreply.github.com>
2023-08-10 21:14:43 +0530

    Update Complete Operating System.cpp

--- Complete Operating System.cpp
+++ Complete Operating System.cpp
@@ -4,3 +4,8 @@ MEMORY MANAGEMENT  -
     But there is problem that if a process of size 1 mb is running in 4mb section 3 mb is wasted so this is called internal fragmentation.
     other problem limitation of problem size. 
     degree of multiprogramming is limited and fixed .
+    This also leads to external fragmentation let say we have 1mb left in 2mb process,1 mb in 4 mb process and 1 mb in 10 mb process we have 3 mb vacant but when a 3 mb
+    process comes into picture there is no room for this process to load.
+    Think of fixed partitioning as a cake divided into some fixed parts.
+  
+   2.Dynamic partitioning : As the name suggest we allocate the memory based on the demand of process size dynamically.This fixed the problem of internal fragmentation 
